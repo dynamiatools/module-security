ca45e79169ccbcbb9d3af2714b87a579440d5e43
Mario Serrano Leones <mario@dynamiasoluciones.com>
2024-05-02 23:27:01 -0500

    fixes and developer experience improvements

--- ui/src/main/java/tools/dynamia/modules/security/ui/listeners/SetupApplicationUserInfoListener.java
+++ ui/src/main/java/tools/dynamia/modules/security/ui/listeners/SetupApplicationUserInfoListener.java
@@ -1,16 +1,20 @@
 package tools.dynamia.modules.security.ui.listeners;
 
-import tools.dynamia.app.ApplicationUserInfo;
+import tools.dynamia.commons.UserInfo;
+import tools.dynamia.integration.Containers;
 import tools.dynamia.integration.sterotypes.Listener;
 import tools.dynamia.modules.security.domain.User;
 import tools.dynamia.modules.security.listeners.LoginListener;
 
 @Listener
 public class SetupApplicationUserInfoListener implements LoginListener {
+
+
     @Override
     public void onLoginSuccess(User user) {
-        try {
-            ApplicationUserInfo userInfo = ApplicationUserInfo.get();
+
+        UserInfo userInfo = Containers.get().findObject(UserInfo.class);
+        if (userInfo != null) {
             userInfo.setId(user.getId());
             userInfo.setUid(user.getUuid());
             userInfo.setDate(user.getCreationDate());
@@ -23,8 +27,6 @@ public class SetupApplicationUserInfoListener implements LoginListener {
             if (user.getPhoto() != null) {
                 userInfo.setImage(user.getPhoto().toURL());
             }
-        } catch (Exception e) {
-
         }
     }
 }
